0bbe2df630bf45a157c654ac11207cd9f8138054
Unknown <petitjean.ma@gmail.com>
2017-02-17 11:06:08 +0100

    Added snapshots to Program 3.1

--- Tests/1D BareBones/1DBareBones.c
+++ Tests/1D BareBones/1DBareBones.c
@@ -11,11 +11,21 @@
 #define SIZE 200
 
 int main(){
+
+	/* declaration for saving snapshots */
+	char basename[80] = "sim", filename[100];
+	int frame = 0;
+	FILE *snapshot;
+
 	double ez[SIZE] = {0.}, hy[SIZE] = {0.}, imp0 = 377.0;
+	/* instanciates int qTime, sets maxTime at 250, instanciates mm*/
 	int qTime, maxTime = 250, mm;
 
 	/* do time stepping */
 	for (qTime = 0; qTime < maxTime; qTime++) {
+
+		/* spatial stepping for B and H */
+
 		/* update magnetic field */
 		for (mm = 0; mm < SIZE - 1; mm++)
 			hy[mm] = hy[mm] + (ez[mm + 1] - ez[mm]) / imp0;
@@ -26,8 +36,24 @@ int main(){
 
 		/* hardwire a source node */
 		ez[0] = exp(-(qTime - 30.) * (qTime - 30.) / 100.);
+		
 
-		printf("%g\n", ez[50]);
+		/* write snapshot if time-step is a multiple of 10 */
+		if (qTime % 10 == 0) {
+			/* construct complete file name and increment frame counter */
+			sprintf(filename, "%s.%d", basename, frame++);
+
+			/* open file */
+			snapshot = fopen(filename, "w");
+
+			/* write data to file */
+			for (mm = 0; mm < SIZE; mm++)
+			fprintf(snapshot, "%g\n", ez[mm]);
+
+			/* close file */
+			fclose(snapshot);
+		}
+
 	} /* end of time-stepping */
 
 	return 0;
